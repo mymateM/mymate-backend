2da322d39ca976fb3314764e79379c904bfd6e8b
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2025-02-05 17:15:14 +0900

    init project

---
+++ src/main/java/com/connect/mymate_backend/MyMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.connect.mymate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connect/mymate_backend/MyMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connect.mymate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
